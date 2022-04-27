bf9462472a3802640f8fd8f3862b4551782faf19
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-04-27 19:21:18 +0200

    MAJ DEF? 27/04

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// slider videos
 function videoUrl(projetperso){
             document.getElementById("slider").src = projetperso;
         }
 
+    const imgs = document.querySelectorAll('.bandeau img');
+    const fullPage = document.querySelector('#fullpage');
         
+    imgs.forEach(img => {
+        img.addEventListener('click', function() {
+            fullPage.style.backgroundImage = 'url(' + img.src + ')';
+            fullPage.style.display = 'block';
+          });
+        });
